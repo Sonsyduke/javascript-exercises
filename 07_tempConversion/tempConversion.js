425f19b5657ef031cdab752251ca6d1209cf8070
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-04 15:45:40 +0200

    Finish tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (num) {
+  const farenheitNum = ((num - 32) * 5) / 9;
+  return Number(farenheitNum.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (num) {
+  const celsuisNum = (num * 9) / 5 + 32;
+  return Number(celsuisNum.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
