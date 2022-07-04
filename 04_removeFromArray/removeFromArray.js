18ba9903f6df1e88e413269d2d6c07f6a51b7101
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-04 13:29:56 +0200

    Finish the removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...nums) {
+  //   const indexNum = arr.indexOf(num);
+  //   const removedNum = arr.splice(indexNum, 1);
+  //   console.log(arr);
+  //   return arr;
+  return arr.filter((item) => !nums.includes(item));
 };
 
 // Do not edit below this line
