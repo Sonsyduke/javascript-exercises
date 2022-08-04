d97f803fc59aaeeb8d5bae5b810065b0576be8ac
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-04 12:11:42 +0200

    Added comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,11 @@
 const palindromes = function (str) {
-  //   const len = str.length;
-  //   for (let i = 0; i < len / 2; i++) {
-  //     if (str[i] !== str[len - 1 - i]) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
+  // To get rid of any special characters, spaces
   const regex = /[^A-Z0-9]/gi;
+  // To change everything to lowercase and replace the special characters or spaces with an empty string
   const lowCapString = str.toLowerCase().replace(regex, "");
+  //   split the lower case string, then reverse it and join it again
   const reverseString = lowCapString.split("").reverse().join("");
+  //   Check if they are the same
   return reverseString === lowCapString;
 };
 
