fe6d34c54452111e6afb662b58cf46bd8fa6f1c5
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-04 11:23:54 +0200

    Finish the sum, power and factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,13 +6,32 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function () {};
+const sum = function (arr) {
+  return arr.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+};
 
-const multiply = function () {};
+const multiply = function (arr) {
+  return arr.reduce((acc, cur) => {
+    return acc * cur;
+  });
+};
 
-const power = function () {};
+const power = function (num1, num2) {
+  return num1 ** num2;
+};
 
-const factorial = function () {};
+const factorial = function (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  for (let i = num - 1; i >= 1; i--) {
+    num = num * i;
+  }
+  return num;
+};
 
 // Do not edit below this line
 module.exports = {
