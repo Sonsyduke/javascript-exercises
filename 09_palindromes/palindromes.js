95179be13d96577978fb2edcf86f41eef6d27a25
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-04 12:08:51 +0200

    Finished with the palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  //   const len = str.length;
+  //   for (let i = 0; i < len / 2; i++) {
+  //     if (str[i] !== str[len - 1 - i]) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  const regex = /[^A-Z0-9]/gi;
+  const lowCapString = str.toLowerCase().replace(regex, "");
+  const reverseString = lowCapString.split("").reverse().join("");
+  return reverseString === lowCapString;
 };
 
 // Do not edit below this line
