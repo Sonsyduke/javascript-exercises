5097e71b93d4e21f72bbfd6256c48b60ee113d01
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-05 11:04:34 +0200

    Finished the findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+// 1. Create a function to get the age of the person
+// 2. Use reduce method to figure out the oldest
+// 2.1 create a variale for the oldest
+// 2.2 create one for the current
 
+function getAge(year, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - year;
+}
+
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge < currentAge ? current : oldest;
+  });
 };
 
 // Do not edit below this line
