18de2bcc023350291c9b5a2c340f512adfd18c87
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-05 09:50:01 +0200

    Finish the Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,10 +3,14 @@ const fibonacci = function (num) {
     num2 = 1,
     sum;
 
-  for (let i = 1; i < num; i++) {
-    sum = num1 + num2;
-    num1 = num2;
-    num2 = sum;
+  if (num < 0) {
+    return "OOPS";
+  } else {
+    for (let i = 1; i < num; i++) {
+      sum = num1 + num2;
+      num1 = num2;
+      num2 = sum;
+    }
   }
   return num2;
 };
