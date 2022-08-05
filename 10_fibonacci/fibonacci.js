de8c065410432954f8441dde5cacfd70f685bbf9
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-05 09:44:26 +0200

    Finish the first test of the fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let num1 = 0,
+    num2 = 1,
+    sum;
 
+  for (let i = 1; i < num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
 };
 
 // Do not edit below this line
